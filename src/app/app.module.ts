12873db8c0699d492738b90b065bfbc85a94d827
Fabiano Cardoso <5637995-w2b@users.noreply.gitlab.com>
2020-08-23 04:18:37 -0300

    commit inicial com telas basicas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './sample/form/form.component';
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './page/home/home.component';
+import { ClienteModule } from './cliente/cliente.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TemplateModule,
+    ClienteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
